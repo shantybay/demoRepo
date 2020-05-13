8ce414175d434f843ba30bcfaa1ece6cdf25bf87
shantybay <scottpressnail@gmail.com>
2020-05-13 10:41:20 -0700

    created a new python which prints two matrices

---
+++ samplescript.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 13 10:32:33 2020
+
+@author: sbarnes
+"""
+
+import numpy as np
+import pandas as pd
+
+
+my_matrix=np.eye(4)
+
+my_second_matrix=np.array[[1,2,3,4],
+                          [2,3,4,6]
+                          [6,7,8,9],
+                          [6,4,2,1]]
+
+print(my_matrix)
+
+print(my_second_matrix)
+
+print("hello")
+
+print("goodbye")
