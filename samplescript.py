3ad3efef3cb1b6db70ce43cd983009e4df74bf43
shantybay <scottpressnail@gmail.com>
2020-05-13 11:18:43 -0700

    changed my two matrices

--- samplescript.py
+++ samplescript.py
@@ -9,11 +9,11 @@ import numpy as np
 import pandas as pd
 
 
-my_matrix=np.eye(4)
+my_matrix=np.eye(5)
 
 my_second_matrix=np.array[[1,2,3,4],
                           [2,3,4,6]
-                          [6,7,8,9],
+                          [7,7,8,8],
                           [6,4,2,1]]
 
 print(my_matrix)
